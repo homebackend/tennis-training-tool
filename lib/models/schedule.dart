38f5856fbf90280eb7ea004e8ff987411d4aa049
Neeraj <neerajcd@gmail.com>
2026-08-03 10:48:22 +0530

    Remove copyright from files

--- lib/models/schedule.dart
+++ lib/models/schedule.dart
@@ -1,6 +1,4 @@
 /*
- * Copyright (c) 2026 Neeraj Jakhar
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
